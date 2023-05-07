fd62ed8eb471572fc25fe7dd0d843e56c23fe91d
Thomas Dokas <tjdokas@gmail.com>
2023-05-07 05:06:35 +0000

    Test basic passing

--- test_basic.cpp
+++ test_basic.cpp
@@ -16,7 +16,38 @@ int main()
     Paxos p3(2, "output2.out", peers);
 
     p1.Start(0, (void*)&p1);
-    assert(1);
+    p2.Start(1, (void*)&p2);
+    p3.Start(2, (void*)&p3);
+    {
+        auto x = p1.Status(0);
+        Fate paxos_status = x.first;
+        assert(paxos_status == Decided);
+    }
+    {
+        auto x = p2.Status(0);
+        Fate paxos_status = x.first;
+        assert(paxos_status == Decided);
+    }
+    {
+        auto x = p2.Status(1);
+        Fate paxos_status = x.first;
+        assert(paxos_status == Decided);
+    }
+    {
+        auto x = p3.Status(0);
+        Fate paxos_status = x.first;
+        assert(paxos_status == Decided);
+    }
+    {
+        auto x = p3.Status(1);
+        Fate paxos_status = x.first;
+        assert(paxos_status == Decided);
+    }
+    {
+        auto x = p3.Status(2);
+        Fate paxos_status = x.first;
+        assert(paxos_status == Decided);
+    }
     std::cout << "passed" << std::endl;
     return 0;
 }
